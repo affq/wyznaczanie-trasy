19290b9beda0c24927c580b0479c8e533aa20216
affq <adrianfabisiewicz@gmail.com>
2024-10-25 13:23:32 +0200

    klasy

--- klasy.py
+++ klasy.py
@@ -1,8 +1,18 @@
-class Graf:
-    pass
-
-class Edge:
-    pass
+from typing import List
 
 class Node:
-    pass
+    def __init__ (self, x: float, y: float, edges: List["Edge"]):
+        self.x = x
+        self.y = y
+        self.edges = edges
+
+class Edge:
+    def __init__(self, from_node: "Node", to_node: "Node", cost: float):
+        self.from_node = from_node
+        self.to_node = to_node
+        self.cost = cost
+
+class Graf:
+    def __init__(self, edges: List[Edge], nodes: List[Node]):
+        self.edges = edges
+        self.nodes = nodes
