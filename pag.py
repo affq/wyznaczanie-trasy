fde0645b300609ceab8bf3492428dad45efe7043
julk4kulk4 <julk4kulk4@gmail.com>
2024-10-25 13:23:15 +0200

    z

--- pag.py
+++ pag.py
@@ -4,6 +4,19 @@ arcpy.env.workspace ="C:\studia\sem5\Pag\pag\pag.gdb"
 
 fc = "C:\studia\sem5\Pag\L4_1_BDOT10k__OT_SKJZ_L.shp"
 
+
 with arcpy.da.SearchCursor(fc, ['OID@', 'SHAPE@']) as cursor:
     for row in cursor:
-        print(f'Id: {row[0]} xy: {row[1]}')
+        
+        # poczatek i koniec odcinka jezdni
+        startPoint = row[1].firstPoint
+        endPoint = row[1].lastPoint
+        
+        #wsp x,y veretxow
+        x1 = startPoint.X
+        y1 = startPoint.Y
+        x2 = endPoint.X
+        y2 = endPoint.Y
+        
+        #długość odcinka
+        length = row[1].length
