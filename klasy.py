a4ec94f0942bcffd4a852c6df4093ac961c629e9
affq <adrianfabisiewicz@gmail.com>
2024-11-02 19:29:23 +0100

    implementacja.

--- klasy.py
+++ klasy.py
@@ -1,42 +1,70 @@
 from typing import List, Dict
 
-class Node:
-    def __init__ (self, x: float, y: float, edges: List["Edge"]):
+road_classes_speed = {
+    "motorway": 1,
+    "trunk": 2,
+    "primary": 3,
+    "secondary": 4,
+    "tertiary": 5,
+    "unclassified": 6,
+    "residential": 7,
+    "service": 8,
+    "track": 9,
+    "pedestrian": 10,
+}
+
+class Wierzcholek:
+    def __init__ (self, id: str, x: float, y: float):
+        self.id = id
         self.x = x
         self.y = y
-        self.edges = edges
+        self.edges = []
+
+    def get_neighbours(self):
+        neighbours = []
+        for edge in self.edges:
+            node = edge.get_end(self)
+            neighbours.append((edge, node))
+        return neighbours
         
-# N1 = Node(1, 1, [Edge(None, None, 1)])
-# N2 = Node(2, 2, [Edge(None, None, 1)])
-# N3 = Node(3, 3, [Edge(None, None, 1)])
-# N4 = Node(4, 4, [Edge(None, None, 1)])
-# N5 = Node(5, 5, [Edge(None, None, 1)])
-# N6 = Node(6, 6, [Edge(None, None, 1)])
-# N7 = Node(7, 7, [Edge(None, None, 1)])
-
-# {
-#     N2: [(N3, 5), (N5, 4)],
-#     N3: [(N2, 1), (N4, 2)],
-# }
-
-class Edge:
-    def __init__(self, from_node: "Node", to_node: "Node", cost: float):
+class Krawedz:
+    def __init__(self, id: str, from_node: Wierzcholek, to_node: Wierzcholek, length: float, road_class: str, direction: str):
+        self.id = id
         self.from_node = from_node
         self.to_node = to_node
-        self.cost = cost
+        self.length = length
+        self.road_class_speed = road_classes_speed[road_class]
+        self.direction = direction
+
+    def get_end(self, node: Wierzcholek):
+        if self.from_node == node:
+            return self.to_node
+        else:    
+            return self.from_node
+    
+    def cost_length(self):
+        return self.length
+    
+    def cost_time(self):
+        return self.length / self.road_class_speed
 
 class Graf:
-    def __init__(self, edges: List[Edge], nodes: List[Node]):
-        self.edges = edges
-        self.nodes = nodes
+    def __init__(self):
+        self.edges = []
+        self.nodes = []
     
-    def add_edge(self, edge: Edge):
+    def add_edge(self, edge: Krawedz):
         self.edges.append(edge)
 
         if edge.from_node not in self.nodes:
             self.nodes.append(edge.from_node)
+        else:
+            edge.from_node.edges.append(edge)
+
         if edge.to_node not in self.nodes:
             self.nodes.append(edge.to_node)
+        else:
+            edge.to_node.edges.append(edge)
     
     def get_node_by_id(self, id: int):
         for node in self.nodes:
@@ -44,12 +72,13 @@ class Graf:
                 return node
         return None
     
-    #na razie precies
-    def get_node_by_xy(self, x: float, y: float):
-        for node in self.nodes:
-            if node.x == x and node.y == y:
-                return node
+    def get_edge_by_id(self, id: int):
+        for edge in self.edges:
+            if edge.id == id:
+                return edge
         return None
+    
+    
         
 
 
