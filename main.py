445d76bef406aebc8978611732b8e57a7d7c4f43
julk4kulk4 <julk4kulk4@gmail.com>
2024-11-07 17:21:25 +0100

    wizualizacja sciezki

--- main.py
+++ main.py
@@ -44,7 +44,40 @@ end_id = "471332,575482"
 
 came_from, cost_so_far = a_star(graf, start_id, end_id,'distance')
 length_a_star = cost_so_far[end_id]
-print(f"Length: {length_a_star} m")
-came_from,cost_so_far = a_star(graf, start_id, end_id,'time')
-time = cost_so_far[end_id]
-print(f"Time: {time} s")
+path = retrieve_path2(came_from, start_id, end_id)
+# print(f"Length: {length_a_star} m")
+# came_from,cost_so_far = a_star(graf, start_id, end_id,'time')
+# time = cost_so_far[end_id]
+# print(f"Time: {time} s")
+
+#----------wizualizacja----------------
+spatial_reference = arcpy.Describe(fc).spatialReference
+
+output_folder= "shp"
+output_name = "path.shp"
+arcpy.management.CreateFeatureclass(output_folder, output_name, "POLYLINE",spatial_reference=spatial_reference)
+edges_list = []
+for i in range(len(path)-1):
+    start_node = path[i] 
+    end_node = path[i + 1] 
+    found_edge = False
+    
+    for edge in graf.edges.values():
+        if (edge.from_node.id == start_node and edge.to_node.id == end_node) or (edge.from_node.id == end_node and edge.to_node.id == start_node):
+            edges_list.append(edge)
+            found_edge = True
+            break
+    if not found_edge:
+        print(f"Nie znaleziono krawędzi między {start_node} i {end_node}")
+        break
+    
+ 
+with arcpy.da.InsertCursor(f"{output_folder}/{output_name}", ["SHAPE@"]) as cursor:
+    for edge in edges_list:
+        start_point = arcpy.Point(edge.from_node.x, edge.from_node.y) 
+        end_point = arcpy.Point(edge.to_node.x, edge.to_node.y)
+        polyline = arcpy.Polyline(arcpy.Array([start_point, end_point])) 
+        cursor.insertRow([polyline])
+
+
+print("SHP done")
