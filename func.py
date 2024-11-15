3df006288cc6f0481db73558b4493be35b8a9941
adrian <adrianfabisiewicz@gmail.com>
2024-11-15 02:00:49 +0100

    :/

--- func.py
+++ func.py
@@ -6,10 +6,10 @@ def distance(x1: float, y1: float, x2: float, y2: float) -> float:
     """
     return math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
 
-def retrieve_path2(prev, a, b):
+def retrieve_path(prev, a, b):
     if b not in prev:
         return []
-    path =[]
+    path = []
     current = b
     
     while current != a:
